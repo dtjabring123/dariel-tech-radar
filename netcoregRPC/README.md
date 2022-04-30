4d0092331312585e106995ed5e10cb86894d661b
Sasa Slankamenac <sasaslankamenac@Sasas-MacBook-Pro.local>
2022-04-30 09:46:11 +0200

    remove ignored files

---
+++ netcoregRPC/README.md
@@ -0,0 +1,8 @@
+# gPRC for .NET
+
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-6.0&tabs=visual-studio-code
+
+`dotnet new grpc -o GrpcGreeter`
+
+https://docs.microsoft.com/en-US/aspnet/core/grpc/troubleshoot
+
---
+++ netcoregRPC/client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+  class Program
+  {
+    // Additional configuration is required to successfully run gRPC on macOS.
+    // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+    static async Task Main(string[] args)
+    {
+
+      // This switch must be set before creating the GrpcChannel/HttpClient.
+      AppContext.SetSwitch(
+          "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+      // The port number(5001) must match the port of the gRPC server.
+      // using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+      var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+      var client = new Greeter.GreeterClient(channel);
+      var reply = await client
+        .SayHelloAsync(
+            new HelloRequest { Name = "GreeterClient" }
+          );
+
+      Console.WriteLine("Greeting: " + reply.Message);
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
